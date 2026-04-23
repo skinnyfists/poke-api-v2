e46d4c6832e1d2ad6ac57750f792b5a1411381a5
Tim <timperoulakis@gmail.com>
2026-04-23 19:45:31 +0200

    add Cache::Proxy in front of cache store, intercepting calls in Fetcher

--- lib/utils/fetcher.rb
+++ lib/utils/fetcher.rb
@@ -21,17 +21,24 @@ class Fetcher
       ErrorHandling.undefined_endpoint(endpoint) unless ENDPOINT_OBJECTS[endpoint]
 
       path = "#{BASE_URI}#{endpoint.to_s.tr('_', '-')}/#{sanitize_query(query)}"
-      call_uri(path).merge(resource_name: endpoint)
+      data = call_uri(path)
+      data.merge(resource_name: endpoint)
     end
 
     def call_uri(path)
+      PokeApi::Cache::Proxy.fetch(path) { raw_fetch(path) }
+    end
+
+    private
+
+    def raw_fetch(path)
       uri  = URI(path)
       resp = Net::HTTP.get(uri)
       JSON.parse(resp, symbolize_names: true).merge(url: path)
     end
 
     def sanitize_query(query)
-      return query unless query.is_a? Hash
+      return query.to_s.downcase unless query.is_a? Hash
 
       query[:limit] ||= 20
       query[:offset] = query[:page] ? query[:limit] * (query[:page] - 1) : (query[:offset] || 0)
